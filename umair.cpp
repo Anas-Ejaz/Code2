12d116f76486ab12afe303550b502c4791514083
umairrahim914 <uk742609@gmail.com>
2025-05-15 19:14:47 +0500

    done

--- umair.cpp
+++ umair.cpp
@@ -1,5 +1,96 @@
 #include <iostream>
+#include <string>
+
 using namespace std;
-int main(){
+
+const int MAX_USERS = 100;
+
+struct User {
+    string username;
+    string password;
+    string email;
+    int phoneno;
+};
+
+User users[MAX_USERS];
+int userCount = 0;
+
+
+bool userExists(const string& username) {
+    for (int i = 0; i < userCount; i++) {
+        if (users[i].username == username) {
+            return true;
+        }
+    }
+    return false;
+}
+
+bool emailExists(const string& email){
+    for(int i = 0; i<userCount; i++) {
+        if(users[i].email == email){
+            return true;
+        }
+    }
+    return false;
+}
+
+
+void signup() {
+    if (userCount >= MAX_USERS) {
+        cout << "User limit reached! \n";
+        return;
+    }
+
+    string username, password,email;
+    int phoneno;
+    cout << "--- Signup Page --- \n";
+
+    do{
+    cout << "Enter username: ";
+    cin >> username;
     
+
+    if (userExists(username)) {
+        cout << "Username already exists. Please enter a different username.\n";
+    }
+
+    } while(userExists(username));
+    
+    do{
+        
+    cout<<"Enter an email: ";
+    cin>> email;
+
+    if(emailExists(email)){
+        cout<< "Email already exists. Please Try another."<<endl;
+    }
+
+    } while(emailExists(email));
+        
+    cout << "Enter password: ";
+    cin >> password;
+
+    cout<<"Enter the phone number to add in this email: ";
+    cin >> phoneno;
+
+    users[userCount].username = username;
+    users[userCount].password = password;
+    users[userCount].email = email;
+    users[userCount].phoneno = phoneno;
+    userCount++;
+
+    cout << "Signup successful!\n";
+}
+
+int main() {
+    char choice;
+    do {
+        signup();
+
+        cout << "Do you want to add another user? (y/n): "<<endl;
+        cin >> choice;
+
+    } while (choice == 'y' || choice == 'Y');
+
+    return 0;
 }
