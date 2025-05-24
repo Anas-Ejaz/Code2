43cf0f2e255d864a7ebfbc440ff5c072ef08ac13
umairrahim914 <uk742609@gmail.com>
2025-05-24 12:48:52 +0500

    u

--- umair.cpp
+++ umair.cpp
@@ -1,96 +1,110 @@
-#include <iostream>
-#include <string>
-
-using namespace std;
-
-const int MAX_USERS = 100;
-
-struct User {
-    string username;
-    string password;
-    string email;
-    int phoneno;
-};
-
-User users[MAX_USERS];
-int userCount = 0;
-
-
-bool userExists(const string& username) {
-    for (int i = 0; i < userCount; i++) {
-        if (users[i].username == username) {
-            return true;
-        }
-    }
-    return false;
-}
-
-bool emailExists(const string& email){
-    for(int i = 0; i<userCount; i++) {
-        if(users[i].email == email){
-            return true;
-        }
-    }
-    return false;
-}
-
-
-void signup() {
-    if (userCount >= MAX_USERS) {
-        cout << "User limit reached! \n";
-        return;
-    }
-
-    string username, password,email;
-    int phoneno;
-    cout << "--- Signup Page --- \n";
-
-    do{
-    cout << "Enter username: ";
-    cin >> username;
-    
-
-    if (userExists(username)) {
-        cout << "Username already exists. Please enter a different username.\n";
-    }
-
-    } while(userExists(username));
-    
-    do{
-        
-    cout<<"Enter an email: ";
-    cin>> email;
-
-    if(emailExists(email)){
-        cout<< "Email already exists. Please Try another."<<endl;
-    }
-
-    } while(emailExists(email));
-        
-    cout << "Enter password: ";
-    cin >> password;
-
-    cout<<"Enter the phone number to add in this email: ";
-    cin >> phoneno;
-
-    users[userCount].username = username;
-    users[userCount].password = password;
-    users[userCount].email = email;
-    users[userCount].phoneno = phoneno;
-    userCount++;
-
-    cout << "Signup successful!\n";
-}
+#include <FL/Fl.H>
+#include <FL/Fl_Window.H>
+#include <FL/Fl_Box.H>
 
 int main() {
-    char choice;
-    do {
-        signup();
-
-        cout << "Do you want to add another user? (y/n): "<<endl;
-        cin >> choice;
-
-    } while (choice == 'y' || choice == 'Y');
-
-    return 0;
+    Fl_Window window(300, 200, "FLTK Test");
+    Fl_Box box(20, 40, 260, 100, "Hello FLTK!");
+    window.end();
+    window.show();
+    return Fl::run();
 }
+
+
+
+// #include <iostream>
+// #include <string>
+
+// using namespace std;
+
+// const int MAX_USERS = 100;
+
+// struct User {
+//     string username;
+//     string password;
+//     string email;
+//     int phoneno;
+// };
+
+// User users[MAX_USERS];
+// int userCount = 0;
+
+
+// bool userExists(const string& username) {
+//     for (int i = 0; i < userCount; i++) {
+//         if (users[i].username == username) {
+//             return true;
+//         }
+//     }
+//     return false;
+// }
+
+// bool emailExists(const string& email){
+//     for(int i = 0; i<userCount; i++) {
+//         if(users[i].email == email){
+//             return true;
+//         }
+//     }
+//     return false;
+// }
+
+
+// void signup() {
+//     if (userCount >= MAX_USERS) {
+//         cout << "User limit reached! \n";
+//         return;
+//     }
+
+//     string username, password,email;
+//     int phoneno;
+//     cout << "--- Signup Page --- \n";
+
+//     do{
+//     cout << "Enter username: ";
+//     cin >> username;
+    
+
+//     if (userExists(username)) {
+//         cout << "Username already exists. Please enter a different username.\n";
+//     }
+
+//     } while(userExists(username));
+    
+//     do{
+        
+//     cout<<"Enter an email: ";
+//     cin>> email;
+
+//     if(emailExists(email)){
+//         cout<< "Email already exists. Please Try another."<<endl;
+//     }
+
+//     } while(emailExists(email));
+        
+//     cout << "Enter password: ";
+//     cin >> password;
+
+//     cout<<"Enter the phone number to add in this email: ";
+//     cin >> phoneno;
+
+//     users[userCount].username = username;
+//     users[userCount].password = password;
+//     users[userCount].email = email;
+//     users[userCount].phoneno = phoneno;
+//     userCount++;
+
+//     cout << "Signup successful!\n";
+// }
+
+// int main() {
+//     char choice;
+//     do {
+//         signup();
+
+//         cout << "Do you want to add another user? (y/n): "<<endl;
+//         cin >> choice;
+
+//     } while (choice == 'y' || choice == 'Y');
+
+//     return 0;
+// }
